9f2e067969b13dda23c2d3c21a101d7c7a29338e
barri <barricadeiscool@outlook.com>
2024-09-26 16:14:28 +0800

    init tiptap

--- src/tiptap/tiptap.jsx
+++ src/tiptap/tiptap.jsx
@@ -1,74 +1,159 @@
-import { EditorProvider } from '@tiptap/react'
-import './styles/tiptap.css'
-import 'react-toastify/dist/ReactToastify.css'
-import PropTypes from 'prop-types'
-import { StarterKit } from './extensions/index'
-import EditorInput from '@/components/EditorInput.jsx'
-import { ToastContainer } from 'react-toastify'
-export const Stiptap = (props) => {
-  const defaultExtensions = [
-    StarterKit
-  ]
-  const finalExtensions = props.extensions || defaultExtensions
-  let { content, outputFormat, getOutput, editable, onBeforeCreate, onCreate, onUpdate, onSelectionUpdate, onTransaction, onFocus, onBlur, onDestroy, ...editorOptions } = props
-  editorOptions = Object.assign({
-    editorProps: {
-      attributes: {
-        class: props.className
-      }
-    },
-    onBeforeCreate: (event) => {
-      onBeforeCreate && onBeforeCreate(event)
-    },
-    onCreate: (event) => {
-      onCreate && onCreate(event)
-    },
-    onUpdate: (event) => {
-      getOutput(
-        outputFormat === 'json' ? JSON.stringify(event.editor.getJSON()) : event.editor.getHTML()
-      )
-      onUpdate && onUpdate(event)
-    },
-    onSelectionUpdate: (event) => {
-      onSelectionUpdate && onSelectionUpdate(event)
-    },
-    onTransaction: (event) => {
-      onTransaction && onTransaction(event)
-    },
-    onFocus: (event) => {
-      onFocus && onFocus(event)
-    },
-    onBlur: (event) => {
-      onBlur && onBlur(event)
-    },
-    onDestroy: (event) => {
-      onDestroy && onDestroy(event)
-    }
-  }, editorOptions)
-  // TODO: 暂时添加, 后续删除slotAfter
-  return (
-    <div>
-      <ToastContainer />
-      <EditorProvider {...editorOptions} slotAfter={<EditorInput content={content} />} extensions={finalExtensions} content={content}></EditorProvider>
-    </div>
-  )
-}
-Stiptap.propTypes = {
-  content: PropTypes.string,
-  outputFormat: PropTypes.oneOf(['json', 'html']),
-  getOutput: PropTypes.func,
-  onBeforeCreate: PropTypes.func,
-  onCreate: PropTypes.func,
-  onUpdate: PropTypes.func,
-  onSelectionUpdate: PropTypes.func,
-  onTransaction: PropTypes.func,
-  onFocus: PropTypes.func,
-  onBlur: PropTypes.func,
-  onDestroy: PropTypes.func,
-  slashMenu: PropTypes.object,
-  extensions: PropTypes.array,
-  className: PropTypes.string,
-  editable: PropTypes.bool
-}
+<template>
+  <editor-content :editor="editor" />
+</template>
 
-export * from '@/tiptap/extensions'
+<script>
+import StarterKit from '@tiptap/starter-kit'
+import { Editor, EditorContent } from '@tiptap/vue-3'
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  watch: {
+    modelValue(value) {
+      // HTML
+      const isSame = this.editor.getHTML() === value
+
+      // JSON
+      // const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
+
+      if (isSame) {
+        return
+      }
+
+      this.editor.commands.setContent(value, false)
+    },
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        StarterKit,
+      ],
+      content: this.modelValue,
+      onUpdate: () => {
+        // HTML
+        this.$emit('update:modelValue', this.editor.getHTML())
+
+        // JSON
+        // this.$emit('update:modelValue', this.editor.getJSON())
+      },
+    })
+  },
+
+  beforeUnmount() {
+    this.editor.destroy()
+  },
+}
+</script>
+
+<style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  :first-child {
+    margin-top: 0;
+  }
+
+  /* List styles */
+  ul,
+  ol {
+    padding: 0 1rem;
+    margin: 1.25rem 1rem 1.25rem 0.4rem;
+
+    li p {
+      margin-top: 0.25em;
+      margin-bottom: 0.25em;
+    }
+  }
+
+  /* Heading styles */
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+    margin-top: 2.5rem;
+    text-wrap: pretty;
+  }
+
+  h1,
+  h2 {
+    margin-top: 3.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  h1 {
+    font-size: 1.4rem;
+  }
+
+  h2 {
+    font-size: 1.2rem;
+  }
+
+  h3 {
+    font-size: 1.1rem;
+  }
+
+  h4,
+  h5,
+  h6 {
+    font-size: 1rem;
+  }
+
+  /* Code and preformatted text styles */
+  code {
+    background-color: var(--purple-light);
+    border-radius: 0.4rem;
+    color: var(--black);
+    font-size: 0.85rem;
+    padding: 0.25em 0.3em;
+  }
+
+  pre {
+    background: var(--black);
+    border-radius: 0.5rem;
+    color: var(--white);
+    font-family: 'JetBrainsMono', monospace;
+    margin: 1.5rem 0;
+    padding: 0.75rem 1rem;
+
+    code {
+      background: none;
+      color: inherit;
+      font-size: 0.8rem;
+      padding: 0;
+    }
+  }
+
+  blockquote {
+    border-left: 3px solid var(--gray-3);
+    margin: 1.5rem 0;
+    padding-left: 1rem;
+  }
+
+  hr {
+    border: none;
+    border-top: 1px solid var(--gray-2);
+    margin: 2rem 0;
+  }
+}
+</style>
